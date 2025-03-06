3de6b06079be766b82cbd982dae7bc196c90464b
Harshit MUNGRA <hrmungra69@gmail.com>
2025-03-06 12:37:24 -0400

    chp 11 done

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,14 +2,14 @@
 import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
-
+ 
+ 
 export default function Search({ placeholder }: { placeholder: string }) {
-  
+ 
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
+ 
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams)
